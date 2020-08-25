815e7d0bc8619e80cc0a635822b60542cf610e01
tracht <marija.trachtenberg@gmail.com>
2020-08-25 14:35:02 +0100

    Resets age to 0 after age 70

--- src/App.js
+++ src/App.js
@@ -1,36 +1,55 @@
 import React, {useState, useEffect} from 'react';
 import './App.css';
 
-let firstMount = false; 
+let born = false; 
 
 function App() {
 
   const [age, setAge] = useState(0);
+  const [title, setTitle] = useState(null);
+  const [nirvana, setNirvana] = useState(false);
 
-  // runs on mounting ONLY
+  // stops age increase
+  // useEffect((title) => {
+  //   if(born){
+  //    setTitle("nirvana attained");
+  //   } 
+  // }, [nirvana])
+
+  // runs on mounting ONLY -> because of empty array
   useEffect(() => {
     console.log("i am born")
   }, []);
 
   // runs on mounting and update
   useEffect(() => {
-    if(firstMount) {
-      console.log("learn");
+
+    // after being born, learn & make mistakes 
+    if(born && age <= 70) {
+      console.log("learn & make mistakes");
     } else {
-      console.log("first mount");
-      firstMount = true; 
+      setAge(0); setTitle("");
+      console.log("component is born");
+      born = true; 
     }
+
+    // after age 70, attain nirvana
+    if (age === 70) { 
+      setNirvana(true)
+      setTitle("nirvana attained") 
+    }
+
   });
 
-
   function ageHandle() {
-    setAge(age + 1)
+    setAge(age + 10)
   };
 
   return (
     <div className="App">
       <header className="App-header">
         <h2> Use Effect </h2>
+        <h3> { title } </h3>
         <h3> Age: {age} </h3>
         <button onClick={ageHandle}> Learn and grow </button>
       </header>
