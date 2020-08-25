deb65eb9c26347f287ad104637e53de65f2a24e7
tracht <marija.trachtenberg@gmail.com>
2020-08-25 13:16:33 +0100

    Creates Age button counter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 
 function App() {
+
+  const [age, setAge] = useState(0);
+
+  // runs on mounting ONLY
+  useEffect(() => {
+    console.log("i am born")
+  }, []);
+
+
+  // runs on mounting and update
+  useEffect(() => {
+    console.log("learn")
+  })
+
+  function ageHandle() {
+    setAge(age + 1)
+  }
+
   return (
     <div className="App">
-
+      <header className="App-header">
+        <h2> Use Effect </h2>
+        <h3> Age: {age} </h3>
+        <button onClick={ageHandle}> Learn and grow </button>
+      </header>
     </div>
   );
 }
