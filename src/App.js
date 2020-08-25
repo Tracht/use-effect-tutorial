44feed7460d4bb4578508c32828d2f02ba2fe87b
tracht <marija.trachtenberg@gmail.com>
2020-08-25 17:10:24 +0100

    Refactors useEffect with age constraints; creates useMemo with goodDeeds counter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, useMemo} from 'react';
+import Child from './child';
 import './App.css';
 
 let born = false; 
 
 function App() {
 
+  const [goodDeeds, setGoodDeeds] = useState(0);
   const [age, setAge] = useState(0);
   const [title, setTitle] = useState(null);
   const [nirvana, setNirvana] = useState(false);
-
-  // stops age increase
-  // useEffect((title) => {
-  //   if(born){
-  //    setTitle("nirvana attained");
-  //   } 
-  // }, [nirvana])
 
   // runs on mounting ONLY -> because of empty array
   useEffect(() => {
@@ -45,14 +40,41 @@ function App() {
     setAge(age + 10)
   };
 
+  function increaseGoodDeeds() {
+    setGoodDeeds(goodDeeds + 1)
+  }
+
+  function decreaseGoodDeeds() {
+    setGoodDeeds(goodDeeds - 1)
+  }
+
+  const memoChild = useMemo(() => {
+    return <Child></Child>
+  }, [goodDeeds])
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2> Use Effect </h2>
-        <h3> { title } </h3>
-        <h3> Age: {age} </h3>
-        <button onClick={ageHandle}> Learn and grow </button>
+        <div>
+          <h2> Use Effect </h2>
+          <h3> { title } </h3>
+          <h3> Age: {age} </h3>
+          <button onClick={ageHandle}> Learn and grow </button>
+        </div>
+
+        <div>
+          <h2> Use Memo</h2>
+          <h3> Good deeds counter: {goodDeeds} </h3>
+          <button onClick={increaseGoodDeeds}> Do a good deed</button>
+          <button onClick={decreaseGoodDeeds}> Do a bad deed</button>
+          <h3> Normal render</h3>
+          {/* <Child></Child> */}
+          <h3>Memo child render</h3>
+          <h4>{memoChild}</h4>
+        </div>
+       
       </header>
+       
     </div>
   );
 }
